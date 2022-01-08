fb1e7949e5988bfc3ecd83129b42fec2496576da
Anika Tibrewala <62497183+anika-001@users.noreply.github.com>
2022-01-08 22:01:26 +0530

    Update Keys.ts

--- Backend/NodeJs/src/support/Keys.ts
+++ Backend/NodeJs/src/support/Keys.ts
@@ -1,6 +1,6 @@
 class Keys{
     public serverkeys = {
-        'MongoURI': "mongodb+srv://isha:<password>@cluster0.vlbmi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        'MongoURI': "mongodb+srv://isha:bXyXvRsXMttOot8j@cluster0.vlbmi.mongodb.net/KrishiBrahmand?retryWrites=true&w=majority",
         'Secretemail': "Vaish Isha Twink",
         'Secretpassword': "Cat Keyboard",
         'SessionSecret': "Vaishtwi Deghba"
